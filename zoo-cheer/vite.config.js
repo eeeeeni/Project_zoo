9eefc4ad5d3a809e41415932a42582b91ae644fe
eeeeeni <djfwkd35@gmail.com>
2025-05-27 17:09:31 +0900

    amplify update2

--- zoo-cheer/vite.config.js
+++ zoo-cheer/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Project_zoo/',
+  // base: '/Project_zoo/', //깃허브 액션 사용시
+  base: '/', //앰플리파이 사용시
   plugins: [react()],
 })
