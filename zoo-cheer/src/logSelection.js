2f7fefbf9e4195c587c3fb75eaef35afeb3b44ba
eeeeeni <djfwkd35@gmail.com>
2025-05-28 10:29:53 +0900

    dynamodb Test2

--- zoo-cheer/src/logSelection.js
+++ zoo-cheer/src/logSelection.js
@@ -7,7 +7,7 @@ export const logSelection = async (formData) => {
   };
 
   try {
-    await fetch('https://{YOUR-LAMBDA-API-ENDPOINT}', {
+    await fetch('https://fpgk21llrl.execute-api.us-east-1.amazonaws.com/dev/log-selection', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(body),
